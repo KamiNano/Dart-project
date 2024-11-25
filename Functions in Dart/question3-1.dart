322a8a273f18013158871fa401279b4f6c667e6c
KamiNano <nathong8806@gmail.com>
2024-11-25 15:17:46 +0700

    f

--- Functions in Dart/question3-1.dart
+++ Functions in Dart/question3-1.dart
@@ -1,10 +1,32 @@
+import 'dart:io';
+
 int maxNumber(List<int> numbers) {
   if (numbers.isEmpty) {
     throw Exception("The list is empty");
-        }
-  return numbers.reduce((a, b) => a > b ? a : b);
   }
+  return numbers.reduce((a, b) => a > b ? a : b);
+}
 void main() {
-  List<int> nums = [3, 7, 2, 9, 5];
-  print("ตัวเลขที่มากที่สุดคือ: ${maxNumber(nums)}");
+  List<int> nums = [];
+  while (true) {
+    stdout.write("กรอกตัวเลข (กด Enter เมื่อเสร็จ หรือพิมพ์ 'done' เพื่อหยุด): ");
+    String? input = stdin.readLineSync();
+    if (input == 'done') {
+      break;
+    }
+
+    try {
+      int num = int.parse(input!);
+      nums.add(num);
+    } catch (e) {
+      print("กรุณากรอกตัวเลขที่ถูกต้อง!");
+    }
+  }
+
+  try {
+    int max = maxNumber(nums);
+    print("ตัวเลขที่มากที่สุดคือ: $max");
+  } catch (e) {
+    print(e);
+  }
 }
